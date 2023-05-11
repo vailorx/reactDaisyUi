95b21a03daed70ef5d6dc48a4dc82f41751093b2
vailorx <94072130+vailorx@users.noreply.github.com>
2023-05-11 14:43:42 +0200

    1 file modified

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,15 @@ import '../App.css';
 export default function Header(props) {
   function roundHcp(props) {
     hcpRounded = Math.round(props.hcp);
+    return hcpRounded;
   }
-  roundHcp();
   return (
     <div className="card w-screen bg-base-100 shadow-xl rounded-none flex flex-row">
       <div id="headerLeftSide" className="bg-blue-900 w-1/2 py-3 px-4 ">
         <h2 className="card-title text-blue-200">R.Mcllroy</h2>
         <div className="flex flex-row">
           <div className="w-1/2 text-blue-200">
-            <p>27.4 (`{console.log(hcpRounded)}`)</p>
+            <p>27.4 (`{roundHcp}`)</p>
           </div>
           <div className="w-1/2">
             <p className="text-white"> P </p>
